d943fc0854806075ea4bdb42c27f0812785d7c42
siroshy <33943138+siroshy@users.noreply.github.com>
2024-04-03 11:29:27 +0300

    first

---
+++ sharpIR.cpp
@@ -0,0 +1,50 @@
+#include "sharpIR.h"
+
+sharpIR::sharpIR(int pin, int sen)
+{
+    switch (sen)
+    {
+    case 1:
+        a = 30.5;
+        b = 2.7;
+        k = 1;
+        break;
+
+    case 2:
+        a = 13.5;
+        b = 0.7;
+        k = 1;
+        break;
+    default:
+        a = 30.5;
+        b = 2.7;
+        k = 1;
+        break;
+    }
+    this->pin = pin;
+    Vmax = 5;
+}
+
+sharpIR::sharpIR(int pin, float a, float b, float k)
+{
+    this->a = a;
+    this->b = b;
+    this->k = k;
+
+    this->pin = pin;
+    Vmax = 5;
+}
+
+float sharpIR::getDist(){
+    float v = convert(Vmax);
+    return a/v - b;
+}
+
+float sharpIR::convert(float Vmax){
+    int v = analogRead(pin);
+    return Vmax/1024 * v;
+}
+
+void sharpIR::setVmax(float Vmax){
+    this->Vmax = Vmax;
+}
---
+++ sharpIR.h
@@ -0,0 +1,32 @@
+#ifndef SHARP_IR
+#define SHARP_IR
+
+#include <Arduino.h>
+
+    const int SHARP_1080 = 1;
+    const int SHARP_440 = 2;
+
+class sharpIR
+{
+    public:
+
+    sharpIR(int pin, int sen);
+
+    sharpIR(int pin, float a, float b, float k);
+
+    float getDist();
+
+    void setVmax(float Vmax);
+
+    private:
+    float a;
+    float k;
+    float b;
+
+    float convert(float Vmax);
+    float Vmax;
+    int pin;
+
+}
+
+#endif
